48e47121bfee845028aeaa970ec997dbdaab281e
Sahil Nishad <sahilroyal91@gmail.com>
2024-01-27 19:39:28 +0530

    files uploaded to git

--- db.js
+++ db.js
@@ -2,6 +2,4 @@ const dotenv=require('dotenv')
 const mongoose=require('mongoose')
 dotenv.config();
 const connection=mongoose.connect(process.env.mongoURL);
-module.exports={
-    connection
-}
+module.exports=connection
