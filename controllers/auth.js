573cd8d11efcdeb5e1dacc05218a40c12557e8aa
Sahil Nishad <sahilroyal91@gmail.com>
2024-01-27 19:30:15 +0530

    files uploaded to git

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,14 @@
-const express=require('express');
-const dotenv=require('dotenv');
-const cors=require('cors')
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
-const app=express.Router();
-const bcrypt=require('bcrypt');
-const jwt=require('jsonwebtoken');
-const UserModel=require('../models/register');
-const RegisterModel=require('../models/register');
-const auth=require('../middlewares/auth.middleware');
-const BlacklistTokenModel = require('../models/blacklistToken');
+const app = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const UserModel = require("../models/register");
+const RegisterModel = require("../models/register");
+const auth = require("../middlewares/auth.middleware");
+const BlacklistTokenModel = require("../models/blacklistToken");
 
 /**
  * @swagger
@@ -19,63 +19,79 @@ const BlacklistTokenModel = require('../models/blacklistToken');
  *   responses:
  *   200:
  *       description: login successful
- *       
- *       
+ *
+ *
  */
 
-app.post('/login',async(req,res)=>{
-    const { email, password } = req.body;
-	console.log(req.body)
+app.post("/login", async (req, res) => {
+	const { email, password } = req.body;
+	console.log(req.body);
 	try {
 		const user = await RegisterModel.findOne({ email });
-		console.log(user)
-		if (user){
+		console.log(user);
+		if (user) {
 			bcrypt.compare(password, user.password, async (err, result) => {
-				console.log(result)
+				console.log(result);
 				if (result) {
-					console.log(result)
-					const token = jwt.sign({ course: "backend", id: user._id }, process.env.secret_key, {
-						expiresIn: "1h",
-					});
+					console.log(result);
+					const token = jwt.sign(
+						{ course: "backend", id: user._id },
+						process.env.secret_key,
+						{
+							expiresIn: "1h",
+						}
+					);
+
+					const refreshToken = jwt.sign(
+						{ course: "backend", id: user._id },
+						process.env.secret_key,
+						{ expiresIn: "7h" }
+					);
+
 					
-					const refreshToken=jwt.sign({course:"backend",id:user._id},process.env.secret_key,{expiresIn:"7h"})
-					res.cookie("token", token,{
-						httpOnly:true,
-						secure:true
+					res.cookie("refreshToken", refreshToken, {
+						httpOnly: true,
+						secure: true,
+						sameSite: "none",
 					});
+	
+					res.cookie("token", token, {
+						httpOnly: true,
+						sameSite: "none",
+						secure: true,
+					});
+
 					res.send({ msg: "login succesful", token: token });
 				}
 			});
-		}
-		else{
-			res.send({msg:"invalid user"})
+		} else {
+			res.send({ msg: "invalid user" });
 		}
 	} catch (error) {
-		res.status(200).send({ msg: "invalid input", message:error.message });
+		res.status(200).send({ msg: "invalid input", message: error.message });
 	}
-   
-})
-app.post("/register",async(req,res)=>{
-    const {name,email,password,gender}=req.body;
-	console.log(name,email,password,gender);
-    try {
+});
+app.post("/register", async (req, res) => {
+	const { name, email, password, gender } = req.body;
+	console.log(name, email, password, gender);
+	try {
 		bcrypt.hash(password, 5, async (err, hash) => {
 			console.log("hashing started");
 			if (err) {
 				res.send("err while hashing the password");
 			} else {
-				const user=new UserModel({name,email,password:hash,gender});
+				const user = new UserModel({ name, email, password: hash, gender });
 				console.log(user);
 				await user.save();
 				res.status(200).send({ msg: "user registered plz login" });
 			}
 		});
-        // const findingUser=await UserModel.findOne({email})
+		// const findingUser=await UserModel.findOne({email})
 		// if(findingUser){
 		// 	return res.status(400).send({msg:"user already exists"})
 		// }
 		// else{
-			
+
 		// }
 		// const hashpassword=bcrypt.hash(password,5)
 		//     const user= new UserModel({
@@ -85,20 +101,20 @@ app.post("/register",async(req,res)=>{
 	} catch (err) {
 		res.status(400).send({ err: err });
 	}
-})
-app.get("/logout",auth,async(req,res)=>{
-	const token=req.cookies.token
-	const refreshToken=req.cookies.refreshToken
-	try{
-		const blacklist=new BlacklistTokenModel({
-			token,refreshToken
-		})
+});
+app.get("/logout", auth, async (req, res) => {
+	const token = req.cookies.token;
+	const refreshToken = req.cookies.refreshToken;
+	try {
+		const blacklist = new BlacklistTokenModel({
+			token,
+			refreshToken,
+		});
 		await blacklist.save();
-		res.send({msg:"logout successful"})
+		res.send({ msg: "logout successful" });
+	} catch (err) {
+		res.send({ err: err, message: err.message });
 	}
-	catch(err){
-		res.send({err:err,message:err.message})
-	}
-})
+});
 
-module.exports=app;
+module.exports = app;
--- index.js
+++ index.js
@@ -1,53 +1,52 @@
-const express=require('express');
-const dotenv=require('dotenv')
+const express = require("express");
+const dotenv = require("dotenv");
 dotenv.config();
-const cookieParser=require('cookie-parser')
-const cors=require('cors')
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
-const  connection=require('./db');
-const auth=require('./controllers/auth');
-const notes=require('./controllers/notes');
-const app=express();
-
+const connection = require("./db");
+const auth = require("./controllers/auth");
+const notes = require("./controllers/notes");
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin:["https://notesapp-rosy.vercel.app"],
-    credentials:true
-}));
+app.use(
+	cors({
+		origin: ["https://notesapp-rosy.vercel.app","http://127.0.0.1:5173"],
+		credentials: true,
+	})
+);
 
 //swagger docs
 const options = {
-    definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'Notes Blog API',
-        version: '1.0.0',
-      },
-    },
-    server: { url: 'http://localhost:8080' },
-    apis: ['./controllers/*.js'], // files containing annotations as above
-  };
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Notes Blog API",
+			version: "1.0.0",
+		},
+	},
+	server: { url: "http://localhost:8080" },
+	apis: ["./controllers/*.js"], // files containing annotations as above
+};
 const openapiSpecification = swaggerJsdoc(options);
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
-app.get("/",(req,res)=>{
-    res.send("home page")
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+app.get("/", (req, res) => {
+	res.send("home page");
 });
-app.use('/auth',auth);
-app.use('/notes',notes);
+app.use("/auth", auth);
+app.use("/notes", notes);
 
-
-
-app.listen(process.env.port,async()=>{
-    try {
-        console.log("server start at 3000");
-        await connection;
-        console.log("connected to db");
-    } catch (error) {
-        console.log(error);
-    }
-})
+app.listen(process.env.port, async () => {
+	try {
+		console.log("server start at 8080");
+		await connection;
+		console.log("connected to db");
+	} catch (error) {
+		console.log(error);
+	}
+});
